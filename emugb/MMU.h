313b773cc04a54ff251df47a3b6ad3c784e2fb41
Henry Southall <hen0104.hs@gmail.com>
2021-12-17 14:56:31 +0000

    remove redundant stuff from mmu/mbcs

--- emugb/MMU.h
+++ emugb/MMU.h
@@ -15,7 +15,6 @@ public:
 	MMU(std::array<uint8_t,256> m_firmware, std::vector<uint8_t> m_ROM);	//initialize with BIOS code and game ROM
 	~MMU();
 
-	//virtual void updateROM(std::vector<uint8_t> newROM);						//Reset memory and write new ROM
 
 	virtual uint8_t read(uint16_t address);										//Read 8-bit value
 	virtual void write(uint16_t address, uint8_t value);						//Write 8-bit value
@@ -23,7 +22,7 @@ public:
 private:
 	std::array<uint8_t, 65536> m_memory = {};
 	std::array<uint8_t,256> m_BIOS;
-	bool m_isInBIOS = true;		//initially bios is enabled so 0x0-0xFF is mapped to BIOS rom. When exiting the BIOS 0xFF50 is written to.. explained further in write()
+	bool m_isInBIOS = true;		
 	void m_DMATransfer(uint8_t base);
 
 };
@@ -38,13 +37,11 @@ public:
 	void write(uint16_t address, uint8_t value);
 private:
 	uint8_t m_bankNumber = 0, m_higherBankBits = 0;
-	bool m_bankSwitchRequired = false;
 	bool m_RAMBanking = false;
 	std::array<uint8_t,65536> m_memory;
-	//std::vector<uint8_t> m_ROM;	//ROM in its entirety for bank switching.
 	std::vector<std::array<uint8_t, 16384>> m_ROMBanks;
 	std::array<uint8_t,256> m_BIOS;
-	bool m_isInBIOS = true;		//initially bios is enabled so 0x0-0xFF is mapped to BIOS rom. When exiting the BIOS 0xFF50 is written to.. explained further in write()
+	bool m_isInBIOS = true;	
 	void m_DMATransfer(uint8_t base);
 };
 
@@ -66,7 +63,7 @@ private:
 	std::vector<std::array<uint8_t, 16384>> m_ROMBanks;
 	std::array<uint8_t, 256> m_BIOS;
 
-	bool m_isInBIOS = true;		//initially bios is enabled so 0x0-0xFF is mapped to BIOS rom. When exiting the BIOS 0xFF50 is written to.. explained further in write()
+	bool m_isInBIOS = true;		
 	void m_DMATransfer(uint8_t base);
 };
 
@@ -82,11 +79,9 @@ private:
 	uint16_t m_bankNumber = 0;
 	uint16_t m_bankNumberHighBit = 0;
 	uint8_t m_ramBankNumber = 0;
-	bool m_bankSwitchRequired = false;
 	std::array<uint8_t, 65536> m_memory;
-	//std::vector<uint8_t> m_ROM;	//ROM in its entirety for bank switching.
 	std::vector<std::array<uint8_t, 16384>> m_ROMBanks;
 	std::array<uint8_t, 256> m_BIOS;
-	bool m_isInBIOS = true;		//initially bios is enabled so 0x0-0xFF is mapped to BIOS rom. When exiting the BIOS 0xFF50 is written to.. explained further in write()
+	bool m_isInBIOS = true;		
 	void m_DMATransfer(uint8_t base);
 };
